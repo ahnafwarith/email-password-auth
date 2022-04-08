062d898976361c4c95c7b5eff4fc5175e6da7181
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-04-08 23:21:09 +0600

    primitive behaviour unsubscribed

--- src/App.js
+++ src/App.js
@@ -4,12 +4,26 @@ import app from './firebase.init'
 
 const auth = getAuth(app)
 
+const showEventEmail = (event) => {
+  console.log(event.target.value)
+}
+
+const showEventPassword = (event) => {
+  console.log(event.target.value)
+}
+
+const handleFormSubmit = event => {
+  console.log('form-submitted');
+  event.preventdefault()
+}
+
 function App() {
   return (
     <div className="App">
-      <form>
-        <input type="text" />
-        <input type="password" />
+      <form onSubmit={handleFormSubmit}>
+        <input onBlur={showEventEmail} type="email" name="" id="" />
+        <input onBlur={showEventPassword} type="password" name="" id="" />
+        <input type="submit" value="submit" />
       </form>
     </div>
   );
