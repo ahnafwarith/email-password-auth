cad018875fb052abcb1bbed24b12e290960de4a0
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-04-09 02:28:57 +0600

    form control invalid

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import './App.css';
-import { getAuth } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import app from './firebase.init'
 import { Form } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
+import { useState } from 'react';
 
 const auth = getAuth(app)
 
-const showEventEmail = (event) => {
-  console.log(event.target.value)
-}
-
-const showEventPassword = (event) => {
-  console.log(event.target.value)
-}
-
-const handleFormSubmit = event => {
-  console.log('form-submitted');
-  event.preventDefault()
-}
-
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const showEventEmail = (event) => {
+    setEmail(event.target.value)
+  }
+
+  const showEventPassword = (event) => {
+    setPassword(event.target.value)
+  }
+
+  const handleFormSubmit = event => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((response) => {
+        console.log(response.user)
+      })
+      .catch(error =>
+        console.error(error)
+      )
+    event.preventDefault()
+  }
+
   return (
     <div>
       <div className='registration w-50 mx-auto mt-5'>
@@ -28,15 +38,18 @@ function App() {
         <Form onSubmit={handleFormSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control onBlur={showEventEmail} type="email" placeholder="Enter email" />
+            <Form.Control onBlur={showEventEmail} type="email" placeholder="Enter email" required />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
+            <Form.Control.Feedback type="invalid">
+              Email please!.
+            </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control onBlur={showEventPassword} type="password" placeholder="Password" />
+            <Form.Control onBlur={showEventPassword} type="password" placeholder="Password" required />
           </Form.Group>
           <Button variant="primary" type="submit">
             Submit
